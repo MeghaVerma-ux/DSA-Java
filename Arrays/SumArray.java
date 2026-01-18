3b0e5126779ec5dbd042c42c9f254f25e21ff361
Megha Verma <meghaverma0315@gmail.com>
2026-01-18 20:16:51 +0530

    Add sum of array solution.

---
+++ Arrays/SumArray.java
@@ -0,0 +1,14 @@
+public class SumArray{
+  public static void main(String[] args){
+    int[] arr = [1, 2, 3, 4, 5];
+
+    int sum = 0;
+
+    for(int x : arr){
+      sum += x;
+    }
+
+    System.out.println("Sum of array: " + sum);
+    
+  }
+}
