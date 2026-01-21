8820b46b62cf3876d5350c4447b321c767f9cb26
Megha Verma <meghaverma0315@gmail.com>
2026-01-21 12:12:42 +0530

    Create MinElement.java

---
+++ Arrays/MinElement.java
@@ -0,0 +1,17 @@
+public class MinElement{
+  public static void main(String[] args){
+    int[] arr = [10, 5, 20, 8];
+
+    int min = arr[0];
+
+    for(int i = 1; i < arr.length; i++){
+      if(arr[i)] < min{
+         min = arr[i];
+        
+      }
+      
+    }
+  System.out.println("Minimum Element: "+ min);
+    
+  }
+}
