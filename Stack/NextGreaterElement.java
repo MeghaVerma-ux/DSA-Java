c551b6f604f2eff4065c17b146737ba4089e7235
Megha Verma <meghaverma0315@gmail.com>
2026-03-28 21:48:23 +0530

    Create NextGreaterElement.java

---
+++ Stack/NextGreaterElement.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class NextGreaterElement{
+  public static int[] nextGreater(int[] arr){
+
+    int n = arr.length;
+    int[] res = new int[n];
+
+    Stack<Integer> stack = new Stack<>();
+
+    for(int i = n - 1; i >= 0; i--){
+
+
+      while(!stack.isEmpty() && stack.peek <= arr[i]){
+        stack.pop();
+      }
+
+      if(stack.isEmpty()){
+        return false;
+      }
+
+      else{
+        result[i] = stack.peek();
+      }
+
+      stack.push(arr[i]);
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args){
+    
+    int[] arr = {2, 1, 2, 4, 3};
+    int[] res = nextGreater(arr);
+
+    System.out.println(Arrays.toString(res));
+  }
+}
