704dc9a775ee5b31920f0209dfcd12530010a759
Megha Verma <meghaverma0315@gmail.com>
2026-03-30 14:59:54 +0530

    Create MinStack.java
    ## Min Stack
    
    ### Problem
    Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
    
    ### Approach
    Use an auxiliary stack to keep track of minimum values.
    
    ### Time Complexity
    All operations: O(1)

---
+++ Stack/MinStack.java
@@ -0,0 +1,35 @@
+import java.util.Scanner ;
+
+public class MinStack {
+
+  Stack<Integer> stack = new stack<>();
+  Stack<Integer> minStack = new Stack<>();
+
+  public void push(int val) {
+
+    stack.push(val);
+
+    if(minStack.isEmpty() || val <= minStack.peek()) {
+      minStack.push(val);
+    }
+  }
+
+  public void pop() {
+
+    if(stack.isEmpty()) return;
+
+    int removed = stack.pop();
+
+    if(removed == minStack.peek()) {
+      minStack.pop();
+    }
+  }
+
+  public int top() {
+    return stack.peek();
+  }
+
+  public int getMin() {
+    return minStack.peek();
+  }
+}
